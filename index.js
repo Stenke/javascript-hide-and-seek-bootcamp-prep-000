f3ff0ac6e18bb3d0331e051bf3f28db9abfd9ae3
Stephen Enke <stephenenke@gmail.com>
2020-09-01 21:45:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document
+  .getElementById('nested')
+  .querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = ((parseInt(lis[i].innerHTML)) + n)
+  }
+}
+
+function deepestChild() {
+  let lis = document.querySelector('#grand-node')
+  let nextLis = lis.children[0];
+  while (nextLis) {
+    lis = nextLis;
+    nextLis = lis.children[0];
+  }
+  return lis
+}
